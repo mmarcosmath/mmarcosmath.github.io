f058147304d1541f77fb1e489e7665d9a4632f6a
mmarcosmath <mmarcos@acad.ifma.edu.br>
2020-07-28 22:52:04 -0300

    initial version

---
+++ precache-manifest.b4c6c615df38dba03048863aecda33d6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b3b16231e3194bee5bde39065e5bbc18",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e444f85dd8d0866b406f",
+    "url": "/static/css/main.233f8488.chunk.css"
+  },
+  {
+    "revision": "3149472f6872b69caf71",
+    "url": "/static/js/2.3abc75d5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.3abc75d5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e444f85dd8d0866b406f",
+    "url": "/static/js/main.9ac4a63f.chunk.js"
+  },
+  {
+    "revision": "5261cbde10bdb357eb80",
+    "url": "/static/js/runtime-main.704f143c.js"
+  }
+]);
